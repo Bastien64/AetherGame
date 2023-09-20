530a42ee32da09949230852e86529878793dc426
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2023-09-20 18:50:20 +0200

    Add files via upload

---
+++ pages/scriptpages.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    setTimeout(function() {
+      document.getElementById("nav-section").style.opacity = 1;
+    }, 1000);
+  };
+
+const carouselSection = document.getElementById("carousel-section");
